28eba3811201362b420a474c87e8f316921bb41a
WUZe514 <18260624577@163.com>
2021-07-30 15:10:06 +0800

    价格服务增加接口

--- src/main/java/com/roncoo/eshop/price/web/controller/ProductPriceController.java
+++ src/main/java/com/roncoo/eshop/price/web/controller/ProductPriceController.java
@@ -12,54 +12,65 @@ import com.roncoo.eshop.price.service.ProductPriceService;
 @RequestMapping("/product-price")
 public class ProductPriceController {
 
-	@Autowired
-	private ProductPriceService productPriceService;
-	
-	@RequestMapping("/add") 
-	@ResponseBody
-	public String add(ProductPrice productPrice) {
-		try {
-			productPriceService.add(productPrice);
-		} catch (Exception e) {
-			e.printStackTrace(); 
-			return "error";
-		}
-		return "success";
-	}
-	
-	@RequestMapping("/update") 
-	@ResponseBody
-	public String update(ProductPrice productPrice) {
-		try {
-			productPriceService.update(productPrice); 
-		} catch (Exception e) {
-			e.printStackTrace(); 
-			return "error";
-		}
-		return "success";
-	}
-	
-	@RequestMapping("/delete") 
-	@ResponseBody
-	public String delete(Long id) {
-		try {
-			productPriceService.delete(id); 
-		} catch (Exception e) {
-			e.printStackTrace(); 
-			return "error";
-		}
-		return "success";
-	}
-	
-	@RequestMapping("/findById") 
-	@ResponseBody
-	public ProductPrice findById(Long id){
-		try {
-			return productPriceService.findById(id);
-		} catch (Exception e) {
-			e.printStackTrace(); 
-		}
-		return new ProductPrice();
-	}
-	
+    @Autowired
+    private ProductPriceService productPriceService;
+
+    @RequestMapping("/add")
+    @ResponseBody
+    public String add(ProductPrice productPrice) {
+        try {
+            productPriceService.add(productPrice);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
+        }
+        return "success";
+    }
+
+    @RequestMapping("/update")
+    @ResponseBody
+    public String update(ProductPrice productPrice) {
+        try {
+            productPriceService.update(productPrice);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
+        }
+        return "success";
+    }
+
+    @RequestMapping("/delete")
+    @ResponseBody
+    public String delete(Long id) {
+        try {
+            productPriceService.delete(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
+        }
+        return "success";
+    }
+
+    @RequestMapping("/findById")
+    @ResponseBody
+    public ProductPrice findById(Long id) {
+        try {
+            return productPriceService.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ProductPrice();
+    }
+
+    @RequestMapping("/findByProductId")
+    @ResponseBody
+    public ProductPrice findByProductId(Long productId) {
+        try {
+            return productPriceService.findByProductId(productId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ProductPrice();
+    }
+
 }
